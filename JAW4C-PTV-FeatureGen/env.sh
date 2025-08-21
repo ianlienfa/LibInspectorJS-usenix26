964276f9337a72a45da98ad2672dc193cd57c87f
enyenl@andrew.cmu.edu <enyenl@andrew.cmu.edu>
2025-08-21 15:23:02 +0000

    temporarily delete feature gen dir

--- JAW4C-PTV-FeatureGen/env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-brew install mysql-client pkg-config
-export PKG_CONFIG_PATH="$(brew --prefix)/opt/mysql-client/lib/pkgconfig"
-export LDFLAGS="-L$(brew --prefix)/lib -L$(brew --prefix)/opt/openssl/lib"
-export CPPFLAGS="-I$(brew --prefix)/include -I$(brew --prefix)/opt/openssl/include" 
-pip3 install ultraimport urllib3==1.26.6 selenium==4.24.0 pandas mysqlclient==2.0.0 python-dotenv pyramid_chameleon
--- JAW4C-PTV-FeatureGen/static/load_reminder.js
+++
@@ -1,2 +0,0 @@
-var load_div = document.getElementById('js-load');
-load_div.innerHTML = 'All libraries are loaded!';
